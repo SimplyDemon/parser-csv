388e26f321c4735fa25c33a0fb3ffb721224a890
Vlad <d_m_n04@mail.ru>
2022-08-25 16:23:22 +0300

    Set encode

--- api-test/index.php
+++ api-test/index.php
@@ -7,5 +7,6 @@ $helper = new Helper();
 $request = json_decode($helper->sendTestCurlToApi(), true);
 $success = $request['success'];
 $message = $request['message'];
+header('Content-Type: application/json;charset=utf-8');
 echo "<p>Success is {$success}</p>";
 echo "<p>Message is {$message}</p>";
